be1fa37f1ca7be272df4f505ebc50f915c9f20a5
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-07-11 00:22:23 +0530

    second commit

--- main.js
+++ main.js
@@ -5,9 +5,41 @@ inputElementobj.addEventListener('keyup',(event)=>{
     if(event.key=='Enter'|| event.keyCode==13){
         let tempVal=inputElementobj.value;
         if(!isNaN(tempVal)){
-            numberOne=parseInt(tempVal);
-            console.log(numberOne+10);
+            if(numberOne==undefined){
+                numberOne=parseInt(tempVal);
+                document.getElementById('num1').textContent=numberOne;
+                inputElementobj.value=0;
+            }else if(numberTwo==undefined){
+                numberTwo=parseInt(tempVal)
+                document.getElementById('num2').textContent=numberTwo;
+                inputElementobj.value=0;
+            }else{
+                alert('insert again');
+                numberOne=undefined;
+                numberTwo=undefined;
+                document.getElementById('num1').textContent='?';
+                document.getElementById('num2').textContent='?';
+            }
         }
         
     }
 });
+
+const execute=(op)=>{
+    if(!numberOne && !numberTwo){
+        alert('Please insert numbers')
+        return;
+    }
+    switch(op){
+        case '+' : operate((numberOne+numberTwo),op);break;
+        case '-' : operate((numberOne-numberTwo),op);break;
+        case '*' : operate((numberOne*numberTwo),op);break;
+        case '/' : operate((numberOne/numberTwo),op);break;
+        case '%' : operate((numberOne%numberTwo),op);;
+    }
+}
+const operate=(result,op)=>{
+    document.getElementById('ans').textContent=result;
+    document.getElementById('op').textContent=op;
+}
+ 
